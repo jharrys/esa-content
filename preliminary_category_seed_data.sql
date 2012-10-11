185c132c872cf4ddd0be0cb5328592c8abfab8fe
John Harris on D91M31M1/Cygwin <john.harris@imail.org>
2012-10-10 23:57:39 -0600

    cleaning up for category data

---
+++ preliminary_category_seed_data.sql
@@ -0,0 +1,260 @@
+Local Distance Voice Services&/Communications
+Mobile-Portable Voice Devices (VOIP)&/Communications
+PBX And Voice Messaging&/Communications
+Telephony Handsets&/Communications
+Wireless Paging Services&/Communications
+Environmental monitoring&/Data Center - Data Closet
+Equipment rack - Data Center&/Data Center - Data Closet
+Equipment rack - Data Closet&/Data Center - Data Closet
+Rack Mounted Power Distribution (PDU)&/Data Center - Data Closet
+Server Remote Console&/Data Center - Data Closet
+Video Surveillance&/Data Center - Data Closet
+Developer Desktop&/Desktops-Laptops-PDA-Combo Devices-Recorders/Hardware - Computers
+Standard Desktop&/Desktops-Laptops-PDA-Combo Devices-Recorders/Hardware - Computers
+Developer Laptop&/Desktops-Laptops-PDA-Combo Devices-Recorders/Hardware - Computers
+Standard Laptop&/Desktops-Laptops-PDA-Combo Devices-Recorders/Hardware - Computers
+HELP2 Service Development Workstation&/Desktops-Laptops-PDA-Combo Devices-Recorders/Hardware - Computers
+Radiology In-Patient Imaging (Diagnostic) Review Workstation&/Desktops-Laptops-PDA-Combo Devices-Recorders/Hardware - Computers
+Statistics Machine&/Desktops-Laptops-PDA-Combo Devices-Recorders/Hardware - Computers
+PDA&/Desktops-Laptops-PDA-Combo Devices-Recorders/PDA-Combination Devices
+Handheld/PDA Phone combinations&/Desktops-Laptops-PDA-Combo Devices-Recorders/PDA-Combination Devices
+Transcription Digital Recorder (eScription)&/Desktops-Laptops-PDA-Combo Devices-Recorders/Recorders
+General Desktops&/Desktops-Laptops-PDA-Combo Devices-Recorders/Operating Systems - Clients
+Handhelds&/Desktops-Laptops-PDA-Combo Devices-Recorders/Operating Systems - Clients
+Laptops&/Desktops-Laptops-PDA-Combo Devices-Recorders/Operating Systems - Clients
+Tablet PC&/Desktops-Laptops-PDA-Combo Devices-Recorders/Operating Systems - Clients
+Anti-Virus&/Desktops-Laptops-PDA-Combo Devices-Recorders/Other Software - Clients
+Desktop Management&/Desktops-Laptops-PDA-Combo Devices-Recorders/Other Software - Clients
+Email Client&/Desktops-Laptops-PDA-Combo Devices-Recorders/Other Software - Clients
+PDA Synchronization&/Desktops-Laptops-PDA-Combo Devices-Recorders/Other Software - Clients
+Media Projector (LCD, DLP)&/Display-Projectors-TV/Hardware Monitors-Projectors
+Radiology In-Patient Imaging-Diagnostic Review Monitor&/Display-Projectors-TV/Hardware Monitors-Projectors
+Accounts Payable&/General Software/Administration and Finance IS
+AR Management Collections&/General Software/Administration and Finance IS
+Asset Management&/General Software/Administration and Finance IS
+Digital Imaging, Storage and Retrieval&/General Software/Administration and Finance IS
+General Ledger&/General Software/Administration and Finance IS
+Human Resources&/General Software/Administration and Finance IS
+Internal Audit&/General Software/Administration and Finance IS
+Outpatient Administrative and Scheduling&/General Software/Administration and Finance IS
+Payroll&/General Software/Administration and Finance IS
+Real Estate Management System&/General Software/Administration and Finance IS
+Time Clock Tracking&/General Software/Administration and Finance IS
+Anatomic Pathology&/General Software/Clinical I.S.
+Blood Bank&/General Software/Clinical I.S.
+Cardiology&/General Software/Clinical I.S.
+Dietary Tracking&/General Software/Clinical I.S.
+Digital Imaging, Storage and Retrieval (non-Radiology)&/General Software/Clinical I.S.
+Enterprise Master Patient Index (EMPI)&/General Software/Clinical I.S.
+Inpatient Encounter Record&/General Software/Clinical I.S.
+Healthcare Data Dictionary&/General Software/Clinical I.S.
+Home Care Patient Information Management&/General Software/Clinical I.S.
+Labor and Delivery&/General Software/Clinical I.S.
+Laboratory&/General Software/Clinical I.S.
+Lifetime Patient Record (EMR)&/General Software/Clinical I.S.
+Mammography&/General Software/Clinical I.S.
+Operating Room Scheduling&/General Software/Clinical I.S.
+Pharmacy Retail&/General Software/Clinical I.S.
+Physical Therapy&/General Software/Clinical I.S.
+Radiology In-Patient Imaging (Diagnostic)&/General Software/Clinical I.S.
+Radiology In-Patient Text&/General Software/Clinical I.S.
+Radiology Dictation&/General Software/Clinical I.S.
+Staff Scheduling&/General Software/Clinical I.S.
+Surgical Scheduling&/General Software/Clinical I.S.
+Transfusion Medicine&/General Software/Clinical I.S.
+General Transcription (w/wo Voice Recognition)&/General Software/Clinical I.S.
+Department Applications&/General Software/Database
+Desktop Applications&/General Software/Database
+Enterprise Applications&/General Software/Database
+Backup and Recovery (Oracle)&/General Software/Database
+Storage Management (Oracle)&/General Software/Database
+Desktop Search&/General Software/Desktop
+Office Suite (word-processing, spreadsheet, presentation)&/General Software/Desktop
+Interface Engine&/General Software/Interface
+Message Encoding&/General Software/Interface
+Network Device Monitor&/General Software/Monitoring
+Oracle Databases&/General Software/Monitoring
+Microsoft SQL Server Databases&/General Software/Monitoring
+Sybase Databases&/General Software/Monitoring
+Web Connectivity Monitoring&/General Software/Monitoring
+Web J2EE Monitoring&/General Software/Monitoring
+Email Server&/General Software/Other Infrastructure
+Enterprise Service Bus&/General Software/Other Infrastructure
+File And Print&/General Software/Other Infrastructure
+Facility Routers - Large&/Network/Hardware - Network
+Application Network Protocol&/Network/Other Network Standards
+Badge Printer-Encoder&/Printer-Scanner-Fax-Barcode
+Barcode Scanner - Lightweight (general purpose)&/Printer-Scanner-Fax-Barcode
+Barcode Scanner - Multifunction (barcode, electronic signature, image capture - general purpose)&/Printer-Scanner-Fax-Barcode
+Barcode Scanner - Clinical (Patient Bedside)&/Printer-Scanner-Fax-Barcode
+Barcode Printer - Standard&/Printer-Scanner-Fax-Barcode
+Black and White Laser Printer&/Printer-Scanner-Fax-Barcode
+Color Laser Printer&/Printer-Scanner-Fax-Barcode
+Dot Matrix Printer&/Printer-Scanner-Fax-Barcode
+Fax&/Printer-Scanner-Fax-Barcode
+Laboratory Print Server&/Printer-Scanner-Fax-Barcode
+Multi-function Device&/Printer-Scanner-Fax-Barcode
+Portable/Mobile Printer&/Printer-Scanner-Fax-Barcode
+Receipt Printer&/Printer-Scanner-Fax-Barcode
+Scanner&/Printer-Scanner-Fax-Barcode
+Server Naming Standard&/Servers/General
+Administration and Finance&/Servers/Hardware - Servers
+General Application&/Servers/Hardware - Servers
+General Database&/Servers/Hardware - Servers
+File and Print&/Servers/Hardware - Servers
+Hardware VPN Server&/Servers/Hardware - Servers
+GE Carecast EMR&/Servers/Hardware - Servers
+HELP In-Patient&/Servers/Hardware - Servers
+Software VPN Server&/Servers/Hardware - Servers
+Web - Application&/Servers/Hardware - Servers
+Web - DMZ&/Servers/Hardware - Servers
+Web - HTTP&/Servers/Hardware - Servers
+Web - Server&/Servers/Hardware - Servers
+Web - Terminal Server&/Servers/Hardware - Servers
+Web - Terminal Server&/Servers/Hardware - Servers
+Administration and Finance&/Servers/Operating Systems - Servers
+General UNIX&/Servers/Operating Systems - Servers
+Internet - DMZ&/Servers/Operating Systems - Servers
+Internet - General&/Servers/Operating Systems - Servers
+Linux Specific (Project Specific Use Only)&/Servers/Operating Systems - Servers
+HP Non-Stop&/Servers/Operating Systems - Servers
+Windows Server&/Servers/Operating Systems - Servers
+Intel Virtualization&/Servers/Other Software - Servers
+AIX Virtualization&/Servers/Other Software - Servers
+Analog Modem&/Servers/Modem
+Fibre Connectors&/Storage/Connectivity
+Fibre Channel Switches&/Storage/Connectivity
+Host Bus Adapter (HBA)&/Storage/Connectivity
+Internet Protocol (iSCSI)&/Storage/Connectivity
+Disk Virtualization&/Storage/On-Line Storage (Tier 1, 2, 3)
+Help Desk Call Management&/General Software/Other Infrastructure
+Instant Messenger Collaboration Tool&/General Software/Other Infrastructure
+Off-site Media Storage&/General Software/Other Infrastructure
+Streaming Media Client&/General Software/Other Infrastructure
+Streaming Media Server&/General Software/Other Infrastructure
+Web Application Delivery&/General Software/Other Infrastructure
+Anti-Virus&/General Software/Security
+Criteria Based Intrusion Detection&/General Software/Security
+Device Network Authentication (DNA)&/General Software/Security
+Forensic Tools&/General Software/Security
+Host Based Intrusion Detection&/General Software/Security
+Master User Directory (MUD)&/General Software/Security
+Mobile Device Security (Laptop/PDA)&/General Software/Security
+Network Based Intrusion Detection&/General Software/Security
+Personal (desktop/laptop) Firewalls For Intermountain&/General Software/Security
+Public Wireless Authorization Server&/General Software/Security
+RADIUS Address Server&/General Software/Security
+Reduced/Single Sign On&/General Software/Security
+Secure Email&/General Software/Security
+Security Management Console&/General Software/Security
+Software VPN&/General Software/Security
+SSL VPN&/General Software/Security
+Strong Authentication&/General Software/Security
+White Hat Audit Tools&/General Software/Security
+Firewall&/General Software/Web and Internet
+HTTP - DMZ&/General Software/Web and Internet
+HTTP - Application&/General Software/Web and Internet
+HTTP - Proxy&/General Software/Web and Internet
+J2EE Application Server&/General Software/Web and Internet
+Portal&/General Software/Web and Internet
+Portal Content Management Server&/General Software/Web and Internet
+SPAM Filtering&/General Software/Web and Internet
+Pornography Filtering&/General Software/Web and Internet
+Internet Email Gateway&/General Software/Web and Internet
+Affiliated Physicians Offices - Small (10 or fewer users)&/Network/Hardware - Network
+Affiliated Physicians Offices - Medium (15-200 users)&/Network/Hardware - Network
+Affiliated Physicians Offices - Large (> 200 users)&/Network/Hardware - Network
+Wireless Access Points (WAP)&/Network/Hardware - Network
+LAN Controllers&/Network/Hardware - Network
+Layer 4-7 (Server load balancing)&/Network/Hardware - Network
+Closet Switches&/Network/Hardware - Network
+Core Switches&/Network/Hardware - Network
+Facility Routers - Small&/Network/Hardware - Network
+Facility Routers - Medium&/Network/Hardware - Network
+Facility Routers - Large&/Network/Hardware - Network
+Communications&/
+Data Center - Data Closet&/
+Desktops-Laptops-PDA-Combo Devices-Recorders&/
+Display-Projectors-TV&/
+General Software&/
+Network&/
+Printer-Scanner-Fax-Barcode&/
+Servers&/
+Storage&/
+Electrical&/
+Miscellaneous&/
+Equipment&/Data Center - Data Closet
+Hardware - Computers&/Desktops-Laptops-PDA-Combo Devices-Recorders
+Hardware - Network&/Network
+Hardware - Servers&/Servers
+PDA/Combination Devices&/Desktops-Laptops-PDA-Combo Devices-Recorders
+Recorders&/Desktops-Laptops-PDA-Combo Devices-Recorders
+Operating Systems - Clients&/Desktops-Laptops-PDA-Combo Devices-Recorders
+Operating Systems - Servers&/Servers
+Other Software - Clients&/Desktops-Laptops-PDA-Combo Devices-Recorders
+Other Software - Servers&/Servers
+Display&/Display-Projectors-TV
+Hardware Monitors-Projectors&/Display-Projectors-TV
+Television&/Display-Projectors-TV
+Administration and Finance IS&/General Software
+Clinical I.S.&/General Software
+Database&/General Software
+Desktop&/General Software
+Interface&/General Software
+Monitoring&/General Software
+Other Infrastructure&/General Software
+Security&/General Software
+Web and Internet&/General Software
+Other Network Standards&/Network
+QOS&/Network
+Modem&/Servers
+Connectivity&/Storage
+On-Line Storage (Tier 1, 2, 3)&/Storage
+Near-Line Storage (Tier 4)&/Storage
+Off-Line Storage (Tier 5)&/Storage
+Non-Stop Virtual Tape&/Storage
+Software - Storage&/Storage
+Wiring Color&/Electrical
+Wiring Distance&/Electrical
+Wiring Standard&/Electrical
+Plugs&/Electrical
+UPS&/Electrical
+General&/Servers
+Audio And Web Conferencing&/Communications
+Audio Conferencing&/Communications
+Call Accounting&/Communications
+Cellular Providers&/Communications
+Emergency Notification Solution&/Communications
+Local Voice Services&/Communications
+Facility SAN Storage&/Storage/On-Line Storage (Tier 1, 2, 3)
+Primary-Alternate Data Centers&/Storage/On-Line Storage (Tier 1, 2, 3)
+Shared File System (multi-node)&/Storage/On-Line Storage (Tier 1, 2, 3)
+Virtual Tape Device&/Storage/Near-Line Storage (Tier 4)
+Tape Device&/Storage/Off-Line Storage (Tier 5)
+Tape Library&/Storage/Off-Line Storage (Tier 5)
+Tape Media&/Storage/Off-Line Storage (Tier 5)
+Virtual Tape&/Storage/Non-Stop Virtual Tape
+Backup Software - Small Facility Only&/Storage/Software - Storage
+Backup Software&/Storage/Software - Storage
+Backup Software - Vaulting&/Storage/Software - Storage
+Hierarchical Storage Management (HSM) Software&/Storage/Software - Storage
+Tape Drive Pooling Software&/Storage/Software - Storage
+Fibre&/Electrical/Wiring Color
+Network&/Electrical/Wiring Color
+Avocent&/Electrical/Wiring Color
+Telecom Voice&/Electrical/Wiring Color
+Power (PDU)&/Electrical/Wiring Color
+Fire Systems&/Electrical/Wiring Color
+Clinical Monitors&/Electrical/Wiring Color
+Nurse Call-Code Blue&/Electrical/Wiring Color
+I.S. LAN&/Electrical/Wiring Color
+Security Locks-Surveillance&/Electrical/Wiring Color
+Plant Engineering System&/Electrical/Wiring Color
+Television Systems&/Electrical/Wiring Color
+Telemetry Monitoring&/Electrical/Wiring Color
+Distributed Antenna System (DAS)&/Electrical/Wiring Color
+Ethernet&/Electrical/Wiring Distance
+Copper Ethernet&/Electrical/Wiring Standard
+Fibre&/Electrical/Wiring Standard
+Medication Storage Cart&/Miscellaneous
+TDM Analog VoIP&/Communications/Telephony Handsets
